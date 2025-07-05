698d8b28fea9afe700f51c4c7f45ffa772fc6742
TheYautja <am.96138669@gmail.com>
2025-07-05 14:11:44 -0300

    teste tailwind

--- lojaVirtual/src/App.jsx
+++ lojaVirtual/src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 
 function App (){
   return(
-    <h1 class="text-3xl font-bold underline">    Hello world!  </h1>
+    <h1 class="text-3xl font-bold underline">Teste Tailwind</h1>
   )
 }
 
