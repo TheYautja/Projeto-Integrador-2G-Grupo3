3903cea5be6b74d9494a9dac4b6f19ac56a36ec4
TheYautja <am.96138669@gmail.com>
2025-07-16 14:52:06 -0300

    link pro produto individual agora funciona

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App (){
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Homepage />} />
-        <Route path="/produto" element={<ProdutoIndividual />} />
+        <Route path="/ProdutoIndividual" element={<ProdutoIndividual />} />
       </Routes>
     </BrowserRouter>
   )
