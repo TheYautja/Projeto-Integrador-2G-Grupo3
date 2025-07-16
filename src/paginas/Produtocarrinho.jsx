528180b1d0aacf3254207f999a94b9ca489b40a5
Sexo <melinawilpert@gmail.com>
2025-07-16 15:25:31 -0300

    ww

---
+++ src/paginas/Produtocarrinho.jsx
@@ -0,0 +1,22 @@
+import './ProdutoCarrinho.css';
+
+function ProdutoCarrinho({ imagem, nome, preco, impacto, ultimaRevisao }) {
+  return (
+    <div className="produto-carrinho">
+      <input type="checkbox" className="checkbox-produto" />
+      <img src={imagem} alt={nome} className="imagem-produto" />
+      <div className="info-produto">
+        <a href="#" className="titulo-produto">{nome}</a>
+        <p className="preco">R$ {preco}</p>
+        <p className="impacto"><strong>Impacto Ambiental</strong><br />{impacto}</p>
+        <p className="revisao">Última reforma: {ultimaRevisao}</p>
+        <div className="acoes">
+          <button className="btn-comprar">Comprar</button>
+          <span className="quantidade">+1</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ProdutoCarrinho;
