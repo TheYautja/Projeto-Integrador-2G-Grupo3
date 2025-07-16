1041305a0d5912844af0490194c9b31f2c578e8c
Sexo <melinawilpert@gmail.com>
2025-07-16 14:47:33 -0300

    s

--- src/paginas/Prdtindividual.jsx
+++ src/paginas/Prdtindividual.jsx
@@ -24,7 +24,7 @@ function Prdtindividual() {
           />
           <div className="flex gap-2 mt-4">
             <img
-              src="/iphone12.jpg"
+              src="/iphone121.png"
               alt="thumb1"
               className="w-1/3 rounded-xl cursor-pointer hover:scale-105 transition"
               onClick={changeImage}
