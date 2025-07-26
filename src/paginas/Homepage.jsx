913d5ada61f68bc51b609c9849409457ce3bb164
TheYautja <am.96138669@gmail.com>
2025-07-26 16:02:08 -0300

    add css

--- src/paginas/Homepage.jsx
+++ src/paginas/Homepage.jsx
@@ -5,26 +5,43 @@ function Homepage (){
     return(
         <div>
             <Header />
-            <div className="bannerHomepage">
-                <p>transforme o velho em novo, Compre e venda com consciencia!</p>
-            </div>
-            <div className="destaquesHomepage">
-                <div className="cartaoDestaque">
-                    <img src="placeholder.png" alt="item1"></img>
-                    <p>descricao</p>
-                    <h5>impacto ambiental</h5>
+
+
+            <section id="hero">
+                <div className="bannerHomepage">
+                    <p>transforme o velho em novo, Compre e venda com consciencia!</p>
+                    <div className="botoesHomepage">
+                        <button>conheca o funcionamento</button>
+                        <button>comece a vender</button>
+                    </div>
                 </div>
-                <div className="cartaoDestaque">
-                    <img src="placeholder.png" alt="item2"></img>
-                    <p>descricao</p>
-                    <h5>impacto ambiental</h5>
+            </section>
+
+
+            <section id="cards">
+                <h2 className="tituloDestaques">destaques</h2>
+                <div className="destaquesHomepage">
+                    <div className="cartaoDestaque">
+                        <img src="placeholder.png" alt="item1"></img>
+                        <p>descricao</p>
+                        <h5>impacto ambiental</h5>
+                    </div>
+                    <div className="cartaoDestaque">
+                        <img src="placeholder.png" alt="item2"></img>
+                        <p>descricao</p>
+                        <h5>impacto ambiental</h5>
+                    </div>
                 </div>
-            </div>
-            <div className="impactoHomepage">
-                <h1>Impacto Coletivo</h1>
-                <p>x kg de carbono evitados</p>
-                <p>0987 itens reaproveitados</p>
-            </div> 
+            </section>
+
+
+            <section id="impacto">
+                <div className="impactoHomepage">
+                    <h1>Impacto Coletivo</h1>
+                    <p>x kg de carbono evitados</p>
+                    <p>0987 itens reaproveitados</p>
+                </div> 
+            </section>
         </div>
     );
 }
