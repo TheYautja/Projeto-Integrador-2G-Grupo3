ce39c84e0eee18c7bb6235587a950c81d6a3d15b
TheYautja <am.96138669@gmail.com>
2025-07-25 22:00:51 -0300

    modificado header e homepage
    links do header agora ficam na horizontal, add banner com placeholder e html dos cards

--- src/paginas/Homepage.jsx
+++ src/paginas/Homepage.jsx
@@ -1,10 +1,30 @@
 import Header from "./Header"
+import "../CSS/Homepage.css"
 
 function Homepage (){
     return(
         <div>
             <Header />
-            <p>teste</p>
+            <div className="bannerHomepage">
+                <p>transforme o velho em novo, Compre e venda com consciencia!</p>
+            </div>
+            <div className="destaquesHomepage">
+                <div className="cartaoDestaque">
+                    <img src="placeholder.png" alt="item1"></img>
+                    <p>descricao</p>
+                    <h5>impacto ambiental</h5>
+                </div>
+                <div className="cartaoDestaque">
+                    <img src="placeholder.png" alt="item2"></img>
+                    <p>descricao</p>
+                    <h5>impacto ambiental</h5>
+                </div>
+            </div>
+            <div className="impactoHomepage">
+                <h1>Impacto Coletivo</h1>
+                <p>x kg de carbono evitados</p>
+                <p>0987 itens reaproveitados</p>
+            </div> 
         </div>
     );
 }
