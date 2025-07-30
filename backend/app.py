7d4116ab4a72419608357c418bdb1bf7d04aaf08
TheYautja <am.96138669@gmail.com>
2025-07-29 22:33:40 -0300

    -back

--- backend/app.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from rotas.produtos import produtos_bp
-
-app = Flask(__name__)
-app.register_blueprint(produtos_bp , url_prefix="/produtos")
-
-if __name__ == "__main__":
-    app.run (debug=True)
--- backend/criardb.py
+++
@@ -1,4 +0,0 @@
-from db import base, engine
-from modelos import Produto
-
-base.metadata.create_all(bind=engine)
