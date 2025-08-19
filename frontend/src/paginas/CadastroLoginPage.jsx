29cfa899e4d92a82913b90b7ed4b51dfe6c8a083
Luísa <emmanuellepaolasilva@gmail.com>
2025-08-19 10:55:54 -0300

    Update CadastroLoginPage.jsx

--- frontend/src/paginas/CadastroLoginPage.jsx
+++ frontend/src/paginas/CadastroLoginPage.jsx
@@ -2,88 +2,68 @@ import React, { useState } from 'react';
 import Header from './Header';
 import '../CSS/CadastroLoginPage.css';
 
-export default function CadastroPage() {
-  const [cadastro, setCadastro] = useState({
-    nome: '',
-    localizacao: '',
-    email: '',
-    cpf: '',
-    senha: ''
-  });
-
-  const handleCadastroChange = (e) => {
-    const { name, value } = e.target;
-    setCadastro({ ...cadastro, [name]: value });
-  };
-
-  const handleCadastroSubmit = (e) => {
-    e.preventDefault();
-    console.log('Cadastro realizado:', cadastro);
-  };
-
+export default function CadastroLoginPage() {
   return (
-    <div className="page">
-      <Header />
-
-      <main className="center">
-        <section className="card">
-          <div className="avatarWrap">
-            <img className="avatar" src="/assests/icon.png" alt="Usuário" />
-            <img className="avatarPlus" src="/add-circle.svg" alt="Adicionar" />
+    <div className="cadastro-login-container">
+      
+      {/* Cadastro */}
+      <div className="form-box">
+        <h2 className="titulo">Cadastro</h2>
+        <div className="form-card">
+          {/* Ícone */}
+          <div className="icon-wrapper">
+            <div className="icon-circle">
+              <span className="icon-user">👤</span>
+              <div className="icon-add">＋</div>
+            </div>
           </div>
 
-          <h2 className="title">Cadastro</h2>
+          {/* Formulário Cadastro */}
+          <form className="form">
+            <label>Nome completo:</label>
+            <input type="text" />
+            
+            <label>Localização:</label>
+            <input type="text" />
 
-          <form onSubmit={handleCadastroSubmit} className="form">
-            <label className="label">Nome completo:</label>
-            <input
-              className="input"
-              type="text"
-              name="nome"
-              value={cadastro.nome}
-              onChange={handleCadastroChange}
-            />
+            <label>E-mail:</label>
+            <input type="email" />
 
-            <label className="label">Localização:</label>
-            <input
-              className="input"
-              type="text"
-              name="localizacao"
-              value={cadastro.localizacao}
-              onChange={handleCadastroChange}
-            />
+            <label>CPF:</label>
+            <input type="text" />
 
-            <label className="label">E - Mail:</label>
-            <input
-              className="input"
-              type="email"
-              name="email"
-              value={cadastro.email}
-              onChange={handleCadastroChange}
-            />
+            <label>Senha:</label>
+            <input type="password" />
 
-            <label className="label">CPF:</label>
-            <input
-              className="input"
-              type="text"
-              name="cpf"
-              value={cadastro.cpf}
-              onChange={handleCadastroChange}
-            />
-
-            <label className="label">Senha:</label>
-            <input
-              className="input"
-              type="password"
-              name="senha"
-              value={cadastro.senha}
-              onChange={handleCadastroChange}
-            />
-
-            <button type="submit" className="btnPrimary">Cadastrar</button>
+            <button type="submit" className="btn">Cadastrar</button>
           </form>
-        </section>
-      </main>
+        </div>
+      </div>
+
+      {/* Login */}
+      <div className="form-box">
+        <h2 className="titulo">Login</h2>
+        <div className="form-card">
+          {/* Ícone */}
+          <div className="icon-wrapper">
+            <div className="icon-circle">
+              <span className="icon-user">👤</span>
+              <div className="icon-add">＋</div>
+            </div>
+          </div>
+
+          {/* Formulário Login */}
+          <form className="form">
+            <label>E-mail:</label>
+            <input type="email" />
+
+            <label>Senha:</label>
+            <input type="password" />
+
+            <button type="submit" className="btn">Entrar</button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
