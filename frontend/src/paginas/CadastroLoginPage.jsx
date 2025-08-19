525bb164acc213b2a52d12acb2c7e1f2be2ecdc2
Emmanuelle <emmanuellepaolasilva@gmail.com>
2025-08-19 11:00:47 -0300

    Update CadastroLoginPage.jsx

--- frontend/src/paginas/CadastroLoginPage.jsx
+++ frontend/src/paginas/CadastroLoginPage.jsx
@@ -4,65 +4,56 @@ import '../CSS/CadastroLoginPage.css';
 
 export default function CadastroLoginPage() {
   return (
-    <div className="cadastro-login-container">
-      
+    <div className="page">
       {/* Cadastro */}
-      <div className="form-box">
-        <h2 className="titulo">Cadastro</h2>
-        <div className="form-card">
-          {/* Ícone */}
-          <div className="icon-wrapper">
-            <div className="icon-circle">
-              <span className="icon-user">👤</span>
-              <div className="icon-add">＋</div>
-            </div>
-          </div>
-
-          {/* Formulário Cadastro */}
-          <form className="form">
-            <label>Nome completo:</label>
-            <input type="text" />
-            
-            <label>Localização:</label>
-            <input type="text" />
-
-            <label>E-mail:</label>
-            <input type="email" />
-
-            <label>CPF:</label>
-            <input type="text" />
-
-            <label>Senha:</label>
-            <input type="password" />
-
-            <button type="submit" className="btn">Cadastrar</button>
-          </form>
+      <h2 className="titulo">Cadastro</h2>
+      <div className="card">
+        <div className="icone-usuario">
+          <div className="icone"></div>
+          <div className="plus">+</div>
         </div>
+
+        <form className="formulario">
+          <label>Nome completo:</label>
+          <input type="text" />
+
+          <label>Localização:</label>
+          <input type="text" />
+
+          <label>E - Mail:</label>
+          <input type="email" />
+
+          <label>CPF:</label>
+          <input type="text" />
+
+          <label>Senha:</label>
+          <input type="password" />
+
+          <button type="submit" className="botao">
+            Cadastrar
+          </button>
+        </form>
       </div>
 
       {/* Login */}
-      <div className="form-box">
-        <h2 className="titulo">Login</h2>
-        <div className="form-card">
-          {/* Ícone */}
-          <div className="icon-wrapper">
-            <div className="icon-circle">
-              <span className="icon-user">👤</span>
-              <div className="icon-add">＋</div>
-            </div>
-          </div>
-
-          {/* Formulário Login */}
-          <form className="form">
-            <label>E-mail:</label>
-            <input type="email" />
-
-            <label>Senha:</label>
-            <input type="password" />
-
-            <button type="submit" className="btn">Entrar</button>
-          </form>
+      <h2 className="titulo">Login</h2>
+      <div className="card">
+        <div className="icone-usuario">
+          <div className="icone"></div>
+          <div className="plus">+</div>
         </div>
+
+        <form className="formulario">
+          <label>E-mail</label>
+          <input type="email" />
+
+          <label>Senha:</label>
+          <input type="password" />
+
+          <button type="submit" className="botao">
+            Entrar
+          </button>
+        </form>
       </div>
     </div>
   );
